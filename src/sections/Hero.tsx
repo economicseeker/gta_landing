4eeedba36374477b96175298b7f85d94919f6c79
Azad Hüseyin Kılıç <azadhuseyinkilic1@gmail.com>
2025-10-19 16:35:48 +0300

    Add GSAP animations to Hero section and update ComingSoon component with new logo.

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,8 +1,62 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
+import { useMaskSettings } from "../../constants";
 import ComingSoon from "./ComingSoon";
+
 const Hero = () => {
+  const { initialMaskPos, initialMaskSize, maskPos, maskSize } =
+    useMaskSettings();
+
+  useGSAP(() => {
+    gsap.set(".mask-wrapper", {
+      maskPosition: initialMaskPos,
+      maskSize: initialMaskSize,
+    });
+
+    gsap.set(".mask-logo", { marginTop: "-100vh", opacity: 0 });
+
+    gsap.set(".entrance-message", { marginTop: "0vh" });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".hero-section",
+        start: "top top",
+        scrub: 2.5,
+        end: "+=200%",
+        pin: true,
+      },
+    });
+
+    tl.to(".fade-out", { opacity: 0, ease: "power1.inOut" })
+      .to(".scale-out", { scale: 1, ease: "power1.inOut" })
+      .to(".mask-wrapper", { maskSize, ease: "power1.inOut" }, "<")
+      .to(".mask-wrapper", { opacity: 0 })
+      .to(
+        ".overlay-logo",
+        {
+          opacity: 1,
+          onComplete: () => {
+            gsap.to(".overlay-logo", { opacity: 0 });
+          },
+        },
+        "<"
+      )
+      .to(
+        ".entrance-message",
+        {
+          duration: 1,
+          ease: "power1.inOut",
+          maskImage:
+            "radial-gradient(circle at 50% 0vh, black 50%, transparent 100%)",
+        },
+        "<"
+      );
+  });
+
   return (
     <section className="hero-section">
-      <div className="size-ful mask-wrapper">
+      <div className="size-full mask-wrapper">
         <img
           src="/images/hero-bg.webp"
           alt="background"
@@ -34,6 +88,7 @@ const Hero = () => {
       <div className="fake-logo-wrapper">
         <img src="/images/big-hero-text.svg" className="overlay-logo" />
       </div>
+
       <ComingSoon />
     </section>
   );
